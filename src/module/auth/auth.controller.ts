b1aca8d9a060ffd995057037460ecc1f03693778
Dong Hyun <decidere@naver.com>
2025-05-26 18:23:56 +0900

    [Feat] 테스트 로직 구현

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: { id: string; password: string }) {}
+
+  @Post('register')
+  async register(
+    @Body()
+    body: {
+      id: string;
+      password: string;
+      nickname: string;
+      role: string;
+    },
+  ) {}
 }
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: {
+    id: string;
+    password: string;
+    nickname: string;
+    role: string;
+  }) {
+    const { id, password, nickname, role } = data;
+
+    // 중복 확인
+    const existingUser = await this.prisma.user.findUnique({
+      where: { userId: id },
+    });
+    if (existingUser) {
+      throw new BadRequestException('이미 존재하는 사용자 ID입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        userId: id,
+        password: hashedPassword,
+        nickname,
+        role,
+      },
+    });
+
+    return {
+      message: '회원가입 성공',
+      user: {
+        id: newUser.id,
+        userId: newUser.userId,
+        nickname: newUser.nickname,
+      },
+    };
+  }
+
+  // 로그인
+  async login(data: { id: string; password: string }) {
+    const { id, password } = data;
+
+    const user = await this.prisma.user.findUnique({
+      where: { userId: id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('존재하지 않는 사용자입니다.');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new BadRequestException('비밀번호가 올바르지 않습니다.');
+    }
+
+    return {
+      message: '로그인 성공',
+      user: {
+        id: user.id,
+        userId: user.userId,
+        nickname: user.nickname,
+      },
+    };
+  }
+}
