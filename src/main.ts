9aefc16dc2f3fbfcbdbf4fed5dba830208ed50fd
Dong Hyun <decidere@naver.com>
2025-05-26 22:52:44 +0900

    [Fix] prefix 제거

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix('duo');
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
