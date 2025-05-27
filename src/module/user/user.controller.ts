4ccc044999349f265cca06b3bdc74f51eb6c7b21
Dong Hyun <decidere@naver.com>
2025-05-27 23:37:49 +0900

    [Feat] 유저 닉네임 설정 로직 구현

---
+++ src/module/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('nickname')
+  @UseGuards(AuthGuard('jwt'))
+  async setUserNickname(
+    @CurrentUserId() userId: number,
+    @Body() body: { nickname: string },
+  ) {
+    return await this.userService.setUserNickname(userId, body.nickname);
+  }
+}
---
+++ src/module/user/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  // 유저 닉네임 설정
+  async setUserNickname(userId: number, nickname: string) {
+    try {
+      await this.prisma.user.update({
+        where: { id: userId },
+        data: { nickname },
+      });
+
+      return {
+        message: { code: 200, text: '닉네임 설정이 완료되었습니다' },
+        user: { nickname },
+      };
+    } catch (err) {
+      console.error('닉네임 설정 중 에러 발생', err);
+      throw new HttpException(
+        '닉네임 설정 중 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
