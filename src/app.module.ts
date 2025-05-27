c97ba17fa4de726cba6af7df2aec90b62e71f6d1
Dong Hyun <decidere@naver.com>
2025-05-27 23:36:32 +0900

    [Feat] user 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './module/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './module/user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
