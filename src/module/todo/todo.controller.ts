57c43a5e0a12a0ad488ab76f103fd702ea943346
Dong Hyun <decidere@naver.com>
2025-06-03 18:37:46 +0900

    [Feat] 투두 등록 로직 구현

--- src/module/todo/todo.controller.ts
+++ src/module/todo/todo.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  async createTodo(
+    @CurrentUserId() userId: number,
+    @Body() body: { coupleId: number; content: string },
+  ) {
+    return await this.todoService.createTodo(
+      userId,
+      body.coupleId,
+      body.content,
+    );
+  }
 }
--- src/module/todo/todo.service.ts
+++ src/module/todo/todo.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createTodo(userId: number, coupleId: number, content: string) {
+    const todo = await this.prisma.todo.create({
+      data: { coupleId, writerId: userId, content, isDone: false },
+    });
+
+    return {
+      message: { code: 200, text: '투두 등록이 완료되었습니다' },
+      todo,
+    };
+  }
+}
