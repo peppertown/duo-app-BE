d854d853944747f56b1c23aacbf6377e6078a79a
Dong Hyun <decidere@naver.com>
2025-06-05 05:30:14 +0900

    [Feat] 커플 데이터 조회 로직 구현

--- src/module/couple/couple.controller.ts
+++ src/module/couple/couple.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { CoupleService } from './couple.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('couple')
 export class CoupleController {
   constructor(private readonly coupleService: CoupleService) {}
+
+  @Get(':coupleId')
+  @UseGuards(AuthGuard('jwt'))
+  async getCoupleData(@Param('coupleId') coupleId: number) {
+    return await this.coupleService.getCoupleData(coupleId);
+  }
 }
--- src/module/couple/couple.service.ts
+++ src/module/couple/couple.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CoupleService {}
+export class CoupleService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 커플 데이터 조회
+  async getCoupleData(coupleId: number) {
+    const couple = await this.prisma.couple.findUnique({
+      where: { id: coupleId },
+    });
+
+    return {
+      message: { code: 200, text: '커플 데이터 조회에 성공했습니다.' },
+      couple: { id: couple.id, name: couple.name, anniversary: couple.aId },
+    };
+  }
+}
