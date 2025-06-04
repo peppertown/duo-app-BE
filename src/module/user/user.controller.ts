a3aeddc60494ed45926d4b54f40318f6339045c1
Dong Hyun <decidere@naver.com>
2025-06-04 23:21:03 +0900

    [Feat] 유저 프로필 사진 변경 로직 구현

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 import { matchUserDocs, setUserNicknameDocs } from './docs/user.docs';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('user')
 @Controller('user')
@@ -31,6 +39,16 @@ export class UserController {
   @matchUserDocs.response
   async matchUser(@CurrentUserId() userId: number, @Body('code') code: string) {
     return await this.userService.matchUser(userId, code);
+  }
+
+  @Post('image')
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadProfileImage(
+    @CurrentUserId() userId: number,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return await this.userService.uploadProfileImage(userId, file);
   }
 
   // @Post('role')
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,9 +1,13 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { S3Service } from 'src/s3/s3.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly s3: S3Service,
+  ) {}
   // 유저 닉네임 설정
   async setUserNickname(userId: number, nickname: string) {
     try {
@@ -60,6 +64,32 @@ export class UserService {
       console.error('커플 연결 중 에러 발생', err);
       throw new HttpException(
         '커플 연결 중 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async uploadProfileImage(userId: number, file: Express.Multer.File) {
+    try {
+      const result = await this.s3.uploadImageToS3(file, 'profile');
+
+      await this.prisma.user.update({
+        where: { id: userId },
+        data: { profileUrl: result.imageUrl },
+      });
+
+      return {
+        message: { code: 200, text: '프로필 사진 업로드가 완료되었습니다.' },
+        user: { profileUrl: result.imageUrl },
+      };
+    } catch (err) {
+      console.error('프로필 사진 업로드 중 에러 발생', err);
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      throw new HttpException(
+        '프로필 사진 업로드 중 오류가 발생했습니다.',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
