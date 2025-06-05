6c7624af01a2ceb1d4d80e281fc709b398b9908a
Dong Hyun <decidere@naver.com>
2025-06-05 16:28:04 +0900

    [Refactor] 커플 관련 api 예외처리 추가

--- src/module/couple/couple.service.ts
+++ src/module/couple/couple.service.ts
@@ -7,71 +7,112 @@ export class CoupleService {
 
   // 커플 데이터 조회
   async getCoupleData(coupleId: number) {
-    const couple = await this.prisma.couple.findUnique({
-      where: { id: coupleId },
-    });
+    try {
+      const couple = await this.prisma.couple.findUnique({
+        where: { id: coupleId },
+      });
 
-    return {
-      message: { code: 200, text: '커플 데이터 조회에 성공했습니다.' },
-      couple: {
-        id: couple.id,
-        name: couple.name,
-        anniversary: couple.anniversary,
-      },
-    };
+      return {
+        message: { code: 200, text: '커플 데이터 조회에 성공했습니다.' },
+        couple: {
+          id: couple.id,
+          name: couple.name,
+          anniversary: couple.anniversary,
+        },
+      };
+    } catch (err) {
+      console.error('커플 데이터 조회 중 에러 발생', err);
+      throw new HttpException(
+        '커플 데이터 조회 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   // 기념일 설정
   async setAnniversary(userId: number, coupleId: number, anniversary: string) {
-    const auth = await this.confirmCoupleAuth(userId, coupleId);
-    if (!auth) {
-      throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+    try {
+      const auth = await this.confirmCoupleAuth(userId, coupleId);
+      if (!auth) {
+        throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+      }
+
+      const couple = await this.prisma.couple.update({
+        where: { id: coupleId },
+        data: { anniversary: new Date(anniversary) },
+      });
+
+      return {
+        message: { code: 200, text: '기념일 설정이 완료되었습니다.' },
+        couple: {
+          id: couple.id,
+          name: couple.name,
+          anniversary: couple.anniversary,
+        },
+      };
+    } catch (err) {
+      console.error('기념일 설정 중 에러 발생');
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      throw new HttpException(
+        '기념일 설정 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    const couple = await this.prisma.couple.update({
-      where: { id: coupleId },
-      data: { anniversary: new Date(anniversary) },
-    });
-
-    return {
-      message: { code: 200, text: '기념일 설정이 완료되었습니다.' },
-      couple: {
-        id: couple.id,
-        name: couple.name,
-        anniversary: couple.anniversary,
-      },
-    };
   }
 
   // 커플 이름 설정
   async setCoupleName(userId: number, coupleId: number, name: string) {
-    const auth = await this.confirmCoupleAuth(userId, coupleId);
-    if (!auth) {
-      throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+    try {
+      const auth = await this.confirmCoupleAuth(userId, coupleId);
+      if (!auth) {
+        throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+      }
+
+      const couple = await this.prisma.couple.update({
+        where: { id: coupleId },
+        data: { name },
+      });
+
+      return {
+        message: { code: 200, text: '커플 이름 설정이 완료되었습니다.' },
+        couple: {
+          id: couple.id,
+          name: couple.name,
+          anniversary: couple.anniversary,
+        },
+      };
+    } catch (err) {
+      console.error('커플 이름 설정 중 에러 발생', err);
+      if (err instanceof HttpException) {
+        throw err;
+      }
+
+      throw new HttpException(
+        '커플 이름 설정 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    const couple = await this.prisma.couple.update({
-      where: { id: coupleId },
-      data: { name },
-    });
-
-    return {
-      message: { code: 200, text: '커플 이름 설정이 완료되었습니다.' },
-      couple: {
-        id: couple.id,
-        name: couple.name,
-        anniversary: couple.anniversary,
-      },
-    };
   }
 
   // 커플 관련 api 권한 확인
   async confirmCoupleAuth(userId: number, coupleId: number) {
-    const coupleIds = await this.prisma.couple.findUnique({
-      where: { id: coupleId },
-    });
+    try {
+      const coupleIds = await this.prisma.couple.findUnique({
+        where: { id: coupleId },
+      });
 
-    const isValid = coupleIds.aId == userId || coupleIds.bId == userId;
-    return isValid;
+      const isValid = coupleIds.aId == userId || coupleIds.bId == userId;
+      return isValid;
+    } catch (err) {
+      console.error('커플 권한 확인 중 에러 발생', err);
+
+      throw new HttpException(
+        '커플 권한 확인 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
