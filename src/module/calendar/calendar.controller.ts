c79b91d4bb68d6ab96985484d409d78f9f9a1e87
Dong Hyun <decidere@naver.com>
2025-06-05 18:14:56 +0900

    [Feat] 달력 일정 등록 로직 구현

--- src/module/calendar/calendar.controller.ts
+++ src/module/calendar/calendar.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CalendarService } from './calendar.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 
 @Controller('calendar')
 export class CalendarController {
   constructor(private readonly calendarService: CalendarService) {}
+
+  @Post('')
+  @UseGuards(AuthGuard('jwt'))
+  async createSchedule(
+    @CurrentUserId() userId: number,
+    @Body() body: { coupleId: number; date: string; content: string },
+  ) {
+    return await this.calendarService.createSchdule(
+      userId,
+      body.coupleId,
+      body.date,
+      body.content,
+    );
+  }
 }
--- src/module/calendar/calendar.service.ts
+++ src/module/calendar/calendar.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CalendarService {}
+export class CalendarService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createSchdule(
+    userId: number,
+    coupleId: number,
+    date: string,
+    content: string,
+  ) {
+    const res = await this.prisma.schedule.create({
+      data: { userId, date: new Date(date), content, coupleId },
+    });
+
+    return res;
+  }
+}
