2eb185684c2e070ecdbdb1d24c930009a55e2714
Dong Hyun <decidere@naver.com>
2025-06-12 23:19:18 +0900

    [Feat] 메모 조회 로직 구현

--- src/module/memo/memo.controller.ts
+++ src/module/memo/memo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { MemoService } from './memo.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
@@ -6,6 +6,16 @@ import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 @Controller('memo')
 export class MemoController {
   constructor(private readonly memoService: MemoService) {}
+
+  @Get(':coupleId/:memoId')
+  @UseGuards(AuthGuard('jwt'))
+  async getMemo(
+    @CurrentUserId() userId: number,
+    @Param('memoId') memoId: number,
+    @Param('coupleId') coupleId: number,
+  ) {
+    return await this.memoService.getMemo(userId, memoId, coupleId);
+  }
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
--- src/module/memo/memo.service.ts
+++ src/module/memo/memo.service.ts
@@ -9,6 +9,7 @@ export class MemoService {
     private readonly coupleService: CoupleService,
   ) {}
 
+  // 메모 작성
   async createMemo(
     userId: number,
     coupleId: number,
@@ -43,6 +44,43 @@ export class MemoService {
     }
   }
 
+  // 메모 조회
+  async getMemo(userId: number, coupleId: number, memoId: number) {
+    try {
+      const coupleAuth = await this.coupleService.confirmCoupleAuth(
+        userId,
+        coupleId,
+      );
+
+      const memoAuth = await this.confirmMemoAuth(coupleId, memoId);
+
+      if (!coupleAuth || !memoAuth) {
+        throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+      }
+
+      const data = await this.prisma.memoContent.findMany({
+        where: { memoId },
+        orderBy: { id: 'desc' },
+      });
+
+      const memo = this.formatMemo(data);
+
+      return {
+        message: { code: 200, text: '메모 조회가 완료되었습니다.' },
+        memo,
+      };
+    } catch (err) {
+      console.error('메모 조회 중 에러 발생', err);
+      if (err instanceof HttpException) throw err;
+
+      throw new HttpException(
+        '메모 조회 중 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  // 메모 권한 확인
   async confirmMemoAuth(coupleId: number, memoId: number) {
     try {
       const memo = await this.prisma.memo.findUnique({ where: { id: memoId } });
@@ -54,5 +92,15 @@ export class MemoService {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
+  }
+
+  // 메모 포매팅
+  formatMemo(memo: any) {
+    return memo.map((m) => ({
+      id: m.id,
+      writerId: m.writerId,
+      content: m.content,
+      createdAt: m.createdAt,
+    }));
   }
 }
