3dc470be756ef079515aac54a625c74872588c57
Dong Hyun <decidere@naver.com>
2025-06-19 23:44:25 +0900

    [Feat] 메모 생성 로직 구현

--- src/module/memo/memo.controller.ts
+++ src/module/memo/memo.controller.ts
@@ -11,10 +11,19 @@ import { MemoService } from './memo.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { createMemoDocs, deleteMemoDocs, getMemoDocs } from './docs/memo.docs';
 
 @ApiTags('memo')
 @Controller('memo')
 export class MemoController {
   constructor(private readonly memoService: MemoService) {}
+
+  @Post(':coupleId')
+  @UseGuards(AuthGuard('jwt'))
+  async createMemo(
+    @CurrentUserId() userId: number,
+    @Param('coupleId') coupleId: number,
+    @Body('content') content: string,
+  ) {
+    return this.memoService.createMemo(userId, coupleId, content);
+  }
 }
--- src/module/memo/memo.service.ts
+++ src/module/memo/memo.service.ts
@@ -8,4 +8,35 @@ export class MemoService {
     private readonly prisma: PrismaService,
     private readonly coupleService: CoupleService,
   ) {}
+
+  // 메모 생성
+  async createMemo(userId: number, coupleId: number, content: string) {
+    try {
+      const auth = await this.coupleService.confirmCoupleAuth(userId, coupleId);
+      if (!auth) {
+        throw new HttpException('잘못된 접근입니다.', HttpStatus.BAD_REQUEST);
+      }
+
+      const memo = await this.prisma.memo.create({
+        data: {
+          writerId: userId,
+          coupleId,
+          content,
+        },
+      });
+      return {
+        message: { code: 200, text: '메모가 생성되었습니다.' },
+        memo: { id: memo.id },
+      };
+    } catch (err) {
+      console.error('메모 생성 중 에러 발생', err);
+      if (err instanceof HttpException) {
+        throw err;
+      }
+      throw new HttpException(
+        '메모 생성 중 오류가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
