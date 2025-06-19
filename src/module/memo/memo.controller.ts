a6f51f94ba2b86fd224a596f33bbd4fc726b1b06
Dong Hyun <decidere@naver.com>
2025-06-19 23:38:40 +0900

    [Feat] 메모 기능 수정을 위해 DB 수정 및 기본 로직 삭제

--- src/module/memo/memo.controller.ts
+++ src/module/memo/memo.controller.ts
@@ -17,49 +17,4 @@ import { createMemoDocs, deleteMemoDocs, getMemoDocs } from './docs/memo.docs';
 @Controller('memo')
 export class MemoController {
   constructor(private readonly memoService: MemoService) {}
-
-  @Get(':coupleId')
-  @UseGuards(AuthGuard('jwt'))
-  @ApiBearerAuth()
-  @getMemoDocs.operation
-  @getMemoDocs.param1
-  @getMemoDocs.response
-  async getMemo(
-    @CurrentUserId() userId: number,
-    @Param('coupleId') coupleId: number,
-  ) {
-    return await this.memoService.getMemo(userId, coupleId);
-  }
-
-  @Post()
-  @UseGuards(AuthGuard('jwt'))
-  @ApiBearerAuth()
-  @createMemoDocs.operation
-  @createMemoDocs.body
-  @createMemoDocs.response
-  async createMemo(
-    @CurrentUserId() userId: number,
-    @Body() body: { coupleId: number; content: string },
-  ) {
-    return await this.memoService.createMemo(
-      userId,
-      body.coupleId,
-      body.content,
-    );
-  }
-
-  @Delete(':coupleId/:contentId')
-  @UseGuards(AuthGuard('jwt'))
-  @ApiBearerAuth()
-  @deleteMemoDocs.operation
-  @deleteMemoDocs.param1
-  @deleteMemoDocs.param2
-  @deleteMemoDocs.response
-  async deleteMemo(
-    @CurrentUserId() userId: number,
-    @Param('coupleId') coupleId: number,
-    @Param('contentId') contentId: number,
-  ) {
-    return await this.memoService.deleteMemo(userId, coupleId, contentId);
-  }
 }
