589afdf6c4eea5d667a29de892f7d7c85eac53ce
Dong Hyun <decidere@naver.com>
2025-06-25 23:24:21 +0900

    [Feat] DB 수정: 알림 기능 구현을 위한 테이블 생성

--- src/sse/sse.service.ts
+++ src/sse/sse.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
-export class SseService {}
+export class SseService {
+  constructor(private readonly redis: RedisService) {}
+}
