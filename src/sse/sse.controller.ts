2c8ef4c5786ef938122006cad81efe7707e1569c
Dong Hyun <decidere@naver.com>
2025-06-26 00:12:00 +0900

    [Feat] SSE 라우트 구현

--- src/sse/sse.controller.ts
+++ src/sse/sse.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { SseService } from './sse.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
 
 @Controller('sse')
 export class SseController {
   constructor(private readonly sseService: SseService) {}
+
+  @Get('subscribe')
+  @UseGuards(AuthGuard('jwt'))
+  subscribe(@CurrentUserId() userId: number, @Res() res: Response) {
+    this.sseService.subscribe(userId, res);
+  }
 }
--- src/sse/sse.service.ts
+++ src/sse/sse.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
 import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class SseService {
   constructor(private readonly redis: RedisService) {}
+  private clients: Map<number, Response> = new Map();
+
+  subscribe(userId: number, res: Response) {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+
+    this.clients.set(userId, res);
+
+    // 연결 종료 시
+    res.on('close', () => {
+      this.clients.delete(userId);
+    });
+  }
 }
