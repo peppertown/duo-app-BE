b71a8612e4f31b0332beea9b1e7c008e354291a9
Dong Hyun <decidere@naver.com>
2025-06-27 00:09:44 +0900

    [Feat] 알림 삭제 스케줄러 구현

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -6,6 +6,7 @@ import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class CronService {
   constructor(private readonly prisma: PrismaService) {}
+  // 시간 지난 투두 삭제
   @Cron('0 0 * * *')
   async handleDeleteExpiredTodos() {
     try {
@@ -20,6 +21,27 @@ export class CronService {
       console.error('만료된 투두 목록 제거 중 에러 발생', error);
       throw new HttpException(
         '만료된 투두 목록 제거 중 에러가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  // 확인되고 5일 지난 알림 삭제
+  @Cron('0 0 * * *')
+  async handleDeleteExpiredNotifications() {
+    try {
+      const fiveDaysAgo = dayjs().subtract(5, 'day').toDate();
+
+      await this.prisma.notification.deleteMany({
+        where: {
+          isRead: true,
+          createdAt: { lt: fiveDaysAgo },
+        },
+      });
+    } catch (error) {
+      console.error('만료된 알림 제거 중 에러 발생', error);
+      throw new HttpException(
+        '만료된 알림 제거 중 에러가 발생했습니다.',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
