3d30e4d0b0030a69e89ec88cff5e927e8ebcbf5e
Dong Hyun <decidere@naver.com>
2025-06-26 19:44:04 +0900

    [Feat] 알림 조회 로직 구현

--- src/module/notification/notification.controller.ts
+++ src/module/notification/notification.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async getNotifications(@CurrentUserId() userId: number) {
+    return this.notificationService.getNotifications(userId);
+  }
 }
--- src/module/notification/notification.service.ts
+++ src/module/notification/notification.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 알림 조회
+  async getNotifications(userId: number) {
+    try {
+      const result = await this.prisma.notification.findMany({
+        where: { userId },
+      });
+
+      const notifications = result.map(({ userId, ...rest }) => rest);
+
+      // 조회된 알림 읽음 처리
+      await this.prisma.notification.updateMany({
+        where: {
+          userId,
+          isRead: false,
+          id: { gte: result[0].id, lte: result[result.length - 1].id },
+        },
+        data: { isRead: true },
+      });
+
+      return {
+        success: true,
+        message: { code: 200, text: '알림 조회가 완료되었습니다.' },
+        notifications,
+      };
+    } catch (error) {
+      console.error('알림 조회 중 에러 발생', error);
+      throw new HttpException(
+        '알림 조회 중 오류가 발생했습니다..',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
