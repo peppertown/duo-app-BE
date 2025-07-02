893a2803c0b13a4ff1b484f33957944c0f265eb3
Dong Hyun <decidere@naver.com>
2025-07-02 19:51:28 +0900

    [Feat] 마이페이지 조회 로직 구현

--- src/module/mypage/mypage.controller.ts
+++ src/module/mypage/mypage.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { MypageService } from './mypage.service';
+import { CurrentUserId } from 'src/common/decorators/current-user-id.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('mypage')
 export class MypageController {
   constructor(private readonly mypageService: MypageService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  getMypage(@CurrentUserId() userId: number) {
+    return this.mypageService.getMypage(userId);
+  }
 }
--- src/module/mypage/mypage.service.ts
+++ src/module/mypage/mypage.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class MypageService {}
+export class MypageService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 마이페이지 조회
+  async getMypage(userId: number) {
+    const data = await this.prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        couplesAsA: { include: { a: true, b: true } },
+        couplesAsB: { include: { a: true, b: true } },
+      },
+    });
+
+    const partnerData =
+      data.couplesAsA.length > 0 ? data.couplesAsA[0].b : data.couplesAsB[0].a;
+    const user = this.getMypageProfile(data);
+    const partner = this.getMypageProfile(partnerData);
+
+    return { user, partner };
+  }
+
+  getMypageProfile(user: any) {
+    return {
+      id: user.id,
+      nickname: user.nickname,
+      profileUrl: user.profileUrl,
+      bio: user.bio,
+      themeId: user.themeId,
+    };
+  }
+}
