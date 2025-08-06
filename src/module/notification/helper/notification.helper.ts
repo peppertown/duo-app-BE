c9289f65d55f53fef0db404c4bbd118d788217dc
Dong Hyun <decidere@naver.com>
2025-08-06 18:54:09 +0900

    [Feat] notification 헬퍼 로직 구현 : 알림 DB 저장 로직 구현

--- src/module/notification/helper/notification.helper.ts
+++ src/module/notification/helper/notification.helper.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { NotificationType } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -13,5 +14,28 @@ export class NotificationHelper {
     });
 
     return user.pushToken;
+  }
+
+  // 알림 DB에 저장
+  async saveNotification(
+    userId: number,
+    type: NotificationType,
+    payload: { title: string; body: string },
+  ) {
+    try {
+      await this.prisma.notification.create({
+        data: {
+          userId,
+          type,
+          payload: JSON.stringify(payload),
+        },
+      });
+    } catch (err) {
+      console.error('알림 DB 저장 중 에러 발생', err);
+      throw new HttpException(
+        '알림 DB 저장 중 오류가 발생했습니다',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
